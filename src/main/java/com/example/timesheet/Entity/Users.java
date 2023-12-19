735bce4589e25d74012028b51eb8df8c37a30762
Sanjai <sanjai.r@zadroit.com>
2023-12-19 19:23:22 +0530

    "Your descriptive commit message"

--- src/main/java/com/example/timesheet/Entity/Users.java
+++ src/main/java/com/example/timesheet/Entity/Users.java
@@ -45,10 +45,12 @@ public class Users {
     @Column(name = "Passout_Year")
     private Integer passoutYear;
 
+
+    @Column(name = "Reset_Token")
+    private String resetToken;
+
     public Users() {
     }
-/* @Column(name = "Reset_Token")
-    private String resetToken;*/
 
     public Integer getUserId() {
         return userId;
@@ -136,5 +138,13 @@ public class Users {
 
     public void setPassoutYear(Integer passoutYear) {
         this.passoutYear = passoutYear;
+    }
+
+    public String getResetToken() {
+        return resetToken;
+    }
+
+    public void setResetToken(String resetToken) {
+        this.resetToken = resetToken;
     }
 }
